c0dd46375e45fec4fa7fa621d21f9392e1c843e0
MininMobile <miningamer@gmail.com>
2018-06-05 21:17:40 +0100

    :tada:

--- main.cpp
+++ main.cpp
@@ -1,19 +1,37 @@
 #include <string>
 #include <iostream>
+using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	bool running = true;
+	int num1;
+	string operation;
+	int num2;
 
-	while (running) {
-		std::string input;
+	int result;
 
-		std::cout << "Enter first number: ";
+	cout << "Enter first number: ";
+	cin >> num1;
 
-		std::cin >> input;
+	cout << "Enter operation: ";
+	cin >> operation;
 
-		std::cout << "\r\nmy name " << input;
+	cout << "Enter first number: ";
+	cin >> num2;
+
+	if (operation == "+") {
+		result = num1 + num2;
+	} else if (operation == "-") {
+		result = num1 - num2;
+	} else if (operation == "*") {
+		result = num1 * num2;
+	} else if (operation == "/") {
+		result = num1 / num2;
+	} else {
+		return -1;
 	}
+
+	cout << "\r\nResult: " << result << "\r\n\r\n";
 
 	return 0;
 }
